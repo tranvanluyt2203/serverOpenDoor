5a9188d3bf86eaf365384b0178f607ebfc98cc3b
@tranvanluyt22032002 <luyttran@rion-lab.com>
2024-08-21 23:10:43 +0700

    process image to storage

--- APIs/ListUrlApi.py
+++ APIs/ListUrlApi.py
@@ -1,8 +1,11 @@
-from APIs.SettingApi import DOMAIN,API_USER
+from APIs.SettingApi import DOMAIN,API_USER,API_PHOTO
 
 BASE_API = DOMAIN
 
 API_LOGIN = API_USER + "login"
-API_LOGOUT =API_USER + "logout"
-API_CREATE =API_USER + "create"
+API_LOGOUT = API_USER + "logout"
+API_CREATE = API_USER + "create"
+
+API_PUSH_IMAGE = API_PHOTO + "push_image"
+API_GET_IMAGE = API_PHOTO + "get_image"
 
